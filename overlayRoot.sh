6f1f5d53209c41bb34d03cc97fdb33e16e50e4b7
Walter Hüttenmeyer <walter.huettenmeyer@gmail.com>
2019-06-16 22:19:57 +0200

    First edit to get it running on Gentoo
    On Gentoo /dev/ is required to be populated by the kernel devtmpfs before calling init.
    mount --move is added to have it available on the overlay before init.
    
    Added debugging info, as it's hard to debug a script that's running instead of init otherwise.
    Restructured code (especially writing errors, infos and creating directories) to be more readable and compact.

--- overlayRoot.sh
+++ overlayRoot.sh
@@ -50,91 +50,127 @@
 #  To install software, run upgrades and do other changes to the raspberry setup, simply remove the init= 
 #  entry from the cmdline.txt file and reboot, make the changes, add the init= entry and reboot once more. 
 
-fail(){
-	echo -e "$1"
-	/bin/bash
+# give us some debugging, if needed. Set to anything but 0
+DEBUG=1
+
+#check for exit status and print error messages, plus exit to /bin/bash
+checkfail(){
+	if [ $? -ne 0 ]; then
+		printf "[ERROR]\tOverlayroot: $1\n"
+		/bin/bash
+	fi
 }
 
+#print info messages, if debugging is turned on
+info(){
+	if [ $DEBUG -ne 0 ]; then
+	printf "[INFO]\tOverlayroot: $1\n"
+	fi
+}
+
+#create directories and call checkfail()
+makedir(){
+	info "Creating directory $1"
+	mkdir $1
+	checkfail "Could not create $1"
+	}
+	
 # load module
+info "Loading overlay kernel module"
 modprobe overlay
-if [ $? -ne 0 ]; then
-    fail "ERROR: missing overlay kernel module"
-fi
+checkfail "missing overlay kernel module"
+
 # mount /proc
+info "Mounting /proc"
 mount -t proc proc /proc
-if [ $? -ne 0 ]; then
-    fail "ERROR: could not mount proc"
-fi
-# create a writable fs to then create our mountpoints 
+checkfail "could not mount proc"
+
+# create a writable fs to then create our mountpoints
+info "Mounting TMPFS at /mnt"
 mount -t tmpfs inittemp /mnt
-if [ $? -ne 0 ]; then
-    fail "ERROR: could not create a temporary filesystem to mount the base filesystems for overlayfs"
-fi
-mkdir /mnt/lower
-mkdir /mnt/rw
+checkfail "could not create a temporary filesystem to mount the base filesystems for overlayfs"
+
+makedir "/mnt/lower"
+makedir "/mnt/rw"
+checkfail "could not create /mnt/rw"
+
+info "Mounting TMPFS at /mnt/rw"
 mount -t tmpfs root-rw /mnt/rw
-if [ $? -ne 0 ]; then
-    fail "ERROR: could not create tempfs for upper filesystem"
-fi
-mkdir /mnt/rw/upper
-mkdir /mnt/rw/work
-mkdir /mnt/newroot
+checkfail "could not create tempfs for upper filesystem"
+
+makedir "/mnt/rw/upper"
+makedir "/mnt/rw/work"
+makedir /"mnt/newroot"
+
 # mount root filesystem readonly 
 rootDev=`awk '$2 == "/" {print $1}' /etc/fstab`
 rootMountOpt=`awk '$2 == "/" {print $4}' /etc/fstab`
 rootFsType=`awk '$2 == "/" {print $3}' /etc/fstab`
-echo "check if we can locate the root device based on fstab"
+info "rootDev: ${rootDev}"
+info "rootMountOpt: ${rootMountOpt}"
+info "rootFsType: ${rootFsType}"
+info "check if we can locate the root device based on fstab"
 blkid $rootDev
 if [ $? -gt 0 ]; then
-    echo "no success, try if a filesystem with label 'rootfs' is avaialble"
+    info "no success, try if a filesystem with label 'rootfs' is avaialble"
     rootDevFstab=$rootDev
     rootDev=`blkid -L "rootfs"`
     if [ $? -gt 0 ]; then
-        echo "no luck either, try to further parse fstab's root device definition"
-        echo "try if fstab contains a PARTUUID definition"
+        info "no luck either, try to further parse fstab's root device definition"
+        info "try if fstab contains a PARTUUID definition"
         echo "$rootDevFstab" | grep 'PARTUUID=\(.*\)-\([0-9]\{2\}\)'
-        if [ $? -gt 0 ]; then 
-	    fail "could not find a root filesystem device in fstab. Make sure that fstab contains a device definition or a PARTUUID entry for / or that the root filesystem has a label 'rootfs' assigned to it"
-        fi
+        info "rootDevFstab: ${rootDevFstab}"
+	    checkfail "could not find a root filesystem device in fstab. Make sure that fstab contains a device definition or a PARTUUID entry for / or that the root filesystem has a label 'rootfs' assigned to it"
         device=""
         partition=""
         eval `echo "$rootDevFstab" | sed -e 's/PARTUUID=\(.*\)-\([0-9]\{2\}\)/device=\1;partition=\2/'`
         rootDev=`blkid -t "PTUUID=$device" | awk -F : '{print $1}'`p$(($partition))
         blkid $rootDev
-        if [ $? -gt 0 ]; then
-	    fail "The PARTUUID entry in fstab could not be converted into a valid device name. Make sure that fstab contains a device definition or a PARTUUID entry for / or that the root filesystem has a label 'rootfs' assigned to it"
-        fi
+	    checkfail "The PARTUUID entry in fstab could not be converted into a valid device name. Make sure that fstab contains a device definition or a PARTUUID entry for / or that the root filesystem has a label 'rootfs' assigned to it"
     fi
 fi
+
+info "Mounting ${rootDev} at /mnt/lower"
 mount -t ${rootFsType} -o ${rootMountOpt},ro ${rootDev} /mnt/lower
-if [ $? -ne 0 ]; then
-    fail "ERROR: could not ro-mount original root partition"
-fi
+checkfail "could not ro-mount original root partition"
+
+info "Mounting overlay"
 mount -t overlay -o lowerdir=/mnt/lower,upperdir=/mnt/rw/upper,workdir=/mnt/rw/work overlayfs-root /mnt/newroot
-if [ $? -ne 0 ]; then
-    fail "ERROR: could not mount overlayFS"
-fi
+checkfail "could not mount overlayFS"
+
 # create mountpoints inside the new root filesystem-overlay
-mkdir /mnt/newroot/ro
-mkdir /mnt/newroot/rw
+makedir "/mnt/newroot/ro"
+makedir "/mnt/newroot/rw"
+
 # remove root mount from fstab (this is already a non-permanent modification)
+info "Removing root mountpoint from fstab (temporary change already)"
 grep -v "$rootDev" /mnt/lower/etc/fstab > /mnt/newroot/etc/fstab
 echo "#the original root mount has been removed by overlayRoot.sh" >> /mnt/newroot/etc/fstab
 echo "#this is only a temporary modification, the original fstab" >> /mnt/newroot/etc/fstab
 echo "#stored on the disk can be found in /ro/etc/fstab" >> /mnt/newroot/etc/fstab
+
 # change to the new overlay root
+info "Changing to new overlay root"
 cd /mnt/newroot
 pivot_root . mnt
+info "Chrooting"
 exec chroot . sh -c "$(cat <<END
 # move ro and rw mounts to the new root
+printf "[INFO]\tOverlayroot: Mounting /mnt/mnt/lower as /ro\n"
 mount --move /mnt/mnt/lower/ /ro
 if [ $? -ne 0 ]; then
-    echo "ERROR: could not move ro-root into newroot"
+    printf "[ERROR]\tOverlayroot: could not move ro-root into newroot\n"
     /bin/bash
 fi
 mount --move /mnt/mnt/rw /rw
 if [ $? -ne 0 ]; then
-    echo "ERROR: could not move tempfs rw mount into newroot"
+    printf "[ERROR]\tOverlayroot: could not move tempfs rw mount into newroot\n"
+#    /bin/bash
+fi
+printf "[INFO]\tOverlayroot: Moving /mnt/dev to /dev\n"
+mount --move /mnt/dev/ /dev
+if [ $? -ne 0 ]; then
+    printf "[ERROR]\tOverlayroot: could not move dev into newroot\n"
     /bin/bash
 fi
 # unmount unneeded mounts so we can unmout the old readonly root
@@ -143,6 +179,7 @@ umount /mnt/proc
 umount /mnt/dev
 umount /mnt
 # continue with regular init
+printf "[INFO]\tOverlayroot: Executing regular /sbin/init\n"
 exec /sbin/init
 END
 )"
